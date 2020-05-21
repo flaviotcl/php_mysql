b1f814584e3b98774a3fd6062f09e861527c2abc
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-21 20:19:18 -0300

    Listando dados no Banco

--- connections.php
+++ connections.php
@@ -8,7 +8,7 @@ if($conn->connect_errno)
 }
 echo $conn->host_info;
 
-echo "<br>";
+echo "<br>";echo "<br>";
 
 $sql = 'CREATE TABLE products (
                 id INT AUTO_INCREMENT PRIMARY KEY,
@@ -18,10 +18,79 @@ $sql = 'CREATE TABLE products (
 if (!$conn->query($sql))
 {
     echo "Tabela Users já existe !";
+    echo "<br>";
+    echo "<hr>";
+    echo "<br>";
 }
 
-$result = $conn->query('INSERT INTO users (email) VALUE ("flaviotcl@hotmail.com")');
+// $result = $conn->query('INSERT INTO users (email) VALUE ("pablo@yahoo.com")');
 
-var_dump($result);
+$result = $conn->query('SELECT * FROM users');
+
+/** 
+ *  Cada vez que o comando $result->fetch_assoc() é executado ele retorna a 
+ *  linha de registro do Array e dá unset na linha inteira.
+ */
+
+var_dump('ANTES - FETCH_ASSOC',$result->fetch_assoc());
 
 echo "<br>";
+
+echo "<ul>";
+    while($user = $result->fetch_assoc())
+    {
+        echo '<li>'.$user['id'].' - '.$user['email'].'</li>';
+    }
+echo "</ul>";
+
+// print_r($result);
+
+echo "<br>";
+
+var_dump('DEPOIS - FETCH_ASSOC',$result->fetch_assoc());
+
+echo "<br>";
+echo "<hr>";
+echo "<br>";
+
+
+
+
+$result = $conn->query('SELECT * FROM users');
+
+echo "<pre>";
+
+/***
+ *  O comando $result->fetch_all() retorna um array de usuarios e
+ *  e após execução todo o array é esvaziado.  
+ */
+//var_dump('ANTES - FETCH_ALL',$result->fetch_all());
+
+//$users = $result->fetch_all(MYSQLI_ASSOC);
+$users = $result->fetch_all(MYSQLI_NUM);
+echo "<ul>";
+foreach ($users as $user)
+{
+   //echo '<li>'.$user['id'].' - '.$user['email'].'</li>';
+     echo '<li>'.$user[0].' - '.$user[1].'</li>';    
+}
+echo "</ul>";
+
+echo "<br>";
+
+var_dump('DEPOIS - FETCH_ALL',$result->fetch_all());
+
+
+
+echo "<hr>";
+
+
+$users = $conn->query('SELECT * FROM users');
+
+//var_dump($users->fetch_object());
+echo "<ul>";
+while ( $user = $users->fetch_object())
+{
+    echo '<li>'.$user->id.' - '.$user->email.'</li>';    
+}
+echo "</ul>"; 
