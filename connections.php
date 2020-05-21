00c4ceb8fdb60f6eb312f25d7cc910e6e90e5035
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-21 18:40:23 -0300

    Executando comando no Banco de Dados

--- connections.php
+++ connections.php
@@ -7,3 +7,21 @@ if($conn->connect_errno)
     die('Falhou em conectar: ('.$conn->connect_errno.') '.$conn->connect_error);
 }
 echo $conn->host_info;
+
+echo "<br>";
+
+$sql = 'CREATE TABLE products (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                email VARCHAR(255) NOT NULL    
+    )';
+
+if (!$conn->query($sql))
+{
+    echo "Tabela Users já existe !";
+}
+
+$result = $conn->query('INSERT INTO users (email) VALUE ("flaviotcl@hotmail.com")');
+
+var_dump($result);
+
+echo "<br>";
