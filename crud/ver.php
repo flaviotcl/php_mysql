ce6a45d7cc8d7539302d1bb6923dca47ddb90088
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-22 14:19:17 -0300

    visualizando um unico usuário

--- crud/ver.php
+++ crud/ver.php
@@ -1,6 +1,33 @@
 <?php 
+    
+    $conn = require 'conection.php';
 
-var_dump($_GET['id']); 
+    $id = $_GET['id'] ?? 0;
+    
+    if(!0)
+    {
+        $stmt = $conn->prepare('SELECT * FROM users WHERE id = ?');
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $result =  $stmt->get_result();
+        $user = $result->fetch_assoc();        
+    }
+     
+
+    
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>             
+     <h1><?php echo $user['email']; ?></h1>
+     <p>O id deste usuário é <?php echo $user['id']; ?></p>
+     <p><a href="/crud">voltar</a></p>
+</body>
+</html>
